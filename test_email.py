602de6b1aa9d72190aafa5c46da41bfe59391d0a
XMousset <xavier.mousset@live.fr>
2024-11-26 10:00:53 +0100

    Restrictions

--- test_email.py
+++ test_email.py
@@ -8,15 +8,16 @@ from christmas_friends import load_config
 CONFIG_NAME = "config.json"
 TRY_SENDING = False
 
-config = load_config(CONFIG_NAME)
-
 to_me = ChristmasFriend(**{
-    "Name": "Your Name",
-    "Email": "receiver_address@awesome.com",
-    "Exclusions": "",
-    "Gifts ideas": "I would like an orange for Christmas."
-})
+        "Name": "Your Name",
+        "Email": "receiver_address@awesome.com",
+        "Exclusions": "",
+        "Gifts ideas": "I would like an orange for Christmas."
+    })
 
-message = christmas_message(to_me, to_me)
-if TRY_SENDING:
-    send_email(message, to_me, **config)
+if __name__ == "__main__":
+    config = load_config(CONFIG_NAME)
+    message = christmas_message(to_me, to_me)
+    
+    if TRY_SENDING:
+        send_email(message, to_me, **config)
